377b064c936e15a95b47692a16fbd1af277b950e
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-06-21 08:08:16 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeWarsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
