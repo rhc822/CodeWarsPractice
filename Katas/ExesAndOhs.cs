857f2defb0ca9cdb2bccd5291b721850bb871378
Ryan Cunningham <ryan.cunningham@acadiahealthcare.com>
2021-06-24 09:33:43 -0500

    Started/Completed ExesAndOhs kata

---
+++ Katas/ExesAndOhs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsPractice
+{
+    class ExesAndOhs
+    {
+        /*
+        
+        Check to see if a string has the same amount of 'x's and 'o's.The method must return a boolean and be case insensitive.The string can contain any char.
+
+        Examples input/output:
+
+        XO("ooxx") => true
+        XO("xooxx") => false
+        XO("ooxXm") => true
+        XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+        XO("zzoo") => false
+        
+        */
+
+        public static bool ContainsEqualXsAndOs(string myString)
+        {
+            string lowerString = myString.ToLower(); //make everything lowercase
+            int numXs = lowerString.Count(c => c == 'x'); //count the xs, set to variable
+            int numOs = lowerString.Count(c => c == 'o'); //count the xs, set to variable
+            if (numXs == numOs)
+                {
+                    return true;
+                }
+            else
+                {
+                    return false;
+                }
+            //Console.WriteLine($"Number of Xs are: {numXs} and Number of Os are: {numOs}");
+            //return true;
+
+            //count the os, set to variable
+            //compare x and o variables to determine equality, if same return true
+            //else return false
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,15 +7,21 @@ namespace CodeWarsPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a word!");
-            string word = Console.ReadLine();
+            string myString = Console.ReadLine();
+            Console.WriteLine(ExesAndOhs.ContainsEqualXsAndOs(myString));
+            
+            
+            
+            
 
-            // enter a word and set up checks to ensure it is only one word, not phrase or sentence
-            if (word.Contains(" "))
-            {
-                Console.WriteLine("Enter a word!");
-                word = Console.ReadLine();
-            }
-            Console.WriteLine(Isogram.IsIsogram(word));
+
+            // Isogram: enter a word and set up checks to ensure it is only one word, not phrase or sentence
+            //if (word.Contains(" "))
+            //{
+            //    Console.WriteLine("You must enter only a single word without spaces. Please try again.");
+            //    word = Console.ReadLine();
+            //}
+            //Console.WriteLine(Isogram.IsIsogram(word));
         }
 
 
